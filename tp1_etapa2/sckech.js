a49240c65b0c56eea821578e1952faaecb582c82
AdrianoAlexPalomeque <alexpalomeque04@gmail.com>
2025-05-28 19:35:17 -0300

    Update sckech.js

--- tp1_etapa2/sckech.js
+++ tp1_etapa2/sckech.js
@@ -1,17 +1,19 @@
 let particles = [];
+let step = 3; // Tamaño de cada partícula y espaciado
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  
+
   let rects = [
-    { x: 100, y: 100, w: 80, h: 80 },
-    { x: 300, y: 150, w: 120, h: 80 }
+    { x: width / 2 - 140, y: height / 2 - 40, w: 80, h: 80 },
+    { x: width / 2 - 40,  y: height / 2 - 40, w: 80, h: 80 },
+    { x: width / 2 + 60,  y: height / 2 - 40, w: 80, h: 80 }
   ];
-  
+
   for (let r of rects) {
-    for (let x = r.x; x < r.x + r.w; x += 4) {
-      for (let y = r.y; y < r.y + r.h; y += 4) {
+    for (let x = r.x; x < r.x + r.w; x += step) {
+      for (let y = r.y; y < r.y + r.h; y += step) {
         particles.push(new Particle(x, y));
       }
     }
@@ -20,7 +22,7 @@ function setup() {
 
 function draw() {
   background(20);
-  
+
   for (let p of particles) {
     p.update();
     p.show();
@@ -41,26 +43,22 @@ function mouseReleased() {
 
 class Particle {
   constructor(x, y) {
-    this.originX = x;      // Posición original
+    this.originX = x;
     this.originY = y;
-    this.x = x;             // Posición actual
+    this.x = x;
     this.y = y;
-    this.targetX = this.originX;  // Posición destino
-    this.targetY = this.originY;
     this.velX = 0;
     this.velY = 0;
-    this.speed = 6;
+    this.speed = 12;
   }
 
   explode() {
-    // Generar dirección aleatoria hacia fuera del centro
     let angle = atan2(this.originY - height / 2, this.originX - width / 2);
     this.velX = cos(angle) * random(3, this.speed);
     this.velY = sin(angle) * random(3, this.speed);
   }
 
   goBack() {
-    // Cambiar el destino a la posición original
     this.velX = (this.originX - this.x) * 0.1;
     this.velY = (this.originY - this.y) * 0.1;
   }
@@ -68,8 +66,7 @@ class Particle {
   update() {
     this.x += this.velX;
     this.y += this.velY;
-    
-    // Cuando está volviendo, corregimos la velocidad para que no pase de largo
+
     if (!mouseIsPressed) {
       this.velX = (this.originX - this.x) * 0.1;
       this.velY = (this.originY - this.y) * 0.1;
@@ -78,6 +75,6 @@ class Particle {
 
   show() {
     fill(255);
-    rect(this.x, this.y, 3, 3);
+    rect(this.x, this.y, step, step);
   }
 }
